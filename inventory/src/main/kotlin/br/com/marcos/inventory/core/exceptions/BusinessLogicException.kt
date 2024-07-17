50586a08dcf02d5d7bcd61c1fb541da688bc5405
Marcos Ramos <rodrigues.ramos@gmail.com>
2024-07-17 11:31:54 -0300

    add exception

---
+++ inventory/src/main/kotlin/br/com/marcos/inventory/core/exceptions/BusinessLogicException.kt
@@ -0,0 +1,4 @@
+package br.com.marcos.inventory.core.exceptions
+
+class BusinessLogicException(message: String): RuntimeException(message){
+}
---
+++ products/src/main/kotlin/br/com/marcos/products/core/exceptions/BusinessLogicException.kt
@@ -0,0 +1,4 @@
+package br.com.marcos.products.core.exceptions
+
+class BusinessLogicException(message: String) : RuntimeException(message){
+}
